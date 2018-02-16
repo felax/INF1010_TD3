524e1c619249631710d0a16758c0b39d2bd2d40d
felax <montminy.felix@gmail.com>
2018-02-16 11:28:08 -0500

    Alpha

--- ProduitAuxEncheres.cpp
+++ ProduitAuxEncheres.cpp
@@ -6,3 +6,36 @@
 
 #include "ProduitAuxEncheres.h"
 
+ProduitAuxEncheres::ProduitAuxEncheres(Fournisseur& fournisseur, 
+    const string& nom = "outil", int reference = 0, double prix = 0.0, 
+    TypeProduit type = TypeProduitOrdinaire, double prixBase = 0.0, 
+    int identifiantClient = 0)
+    : Produit(fournisseur, nom, reference, prix, type)
+{
+    prixBase_ = prixBase;
+    identifiantClient_ = identifiantClient;
+}
+
+int ProduitAuxEncheres::obtenirIdentifiantClient() const
+{
+    return identifiantClient_;
+}
+
+double ProduitAuxEncheres::obtenirPrixBase() const
+{
+    return prixBase_;
+}
+
+ostream& operator<<(ostream& os, const ProduitAuxEncheres& produit)
+{
+	os  << static_cast<Produit>(produit)
+        << "Prix de base " << produit.obtenirPrixBase << endl
+        << "Identifiant Client " << produit.obtenirIdentifiantClient << endl;
+    return os;
+}
+
+istream& operator>>(istream& is, ProduitAuxEncheres& produit)
+{
+    is  >> static_cast<Produit&>(produit);
+    return is;
+}
--- ProduitAuxEncheres.h
+++ ProduitAuxEncheres.h
@@ -15,8 +15,10 @@ class ProduitAuxEncheres : public Produit
 {
 
 public:
-	ProduitAuxEncheres(Fournisseur& fournisseur,const string& nom = "outil", int reference = 0,
-		               double prix = 0.0,TypeProduit type = TypeProduitOrdinaire);
+	ProduitAuxEncheres(Fournisseur& fournisseur, 
+    const string& nom = "outil", int reference = 0, double prix = 0.0, 
+    TypeProduit type = TypeProduitOrdinaire, double prixBase = 0.0, 
+    int identifiantClient = 0);
 
 		
 	int obtenirIdentifiantClient() const;
